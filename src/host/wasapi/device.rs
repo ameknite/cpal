2858dde7a5460296ef8252e9f4fe3c3accd21e58
AmeKnite <104745335+ameknite@users.noreply.github.com>
2024-03-01 23:18:29 -0600

    PROPVARIANT.as_raw()

--- src/host/wasapi/device.rs
+++ src/host/wasapi/device.rs
@@ -297,7 +297,7 @@ impl Device {
                     DeviceNameError::from(err)
                 })?;
 
-            let prop_variant = &property_value.Anonymous.Anonymous;
+            let prop_variant = &property_value.as_raw().Anonymous.Anonymous;
 
             // Read the friendly-name from the union data field, expecting a *const u16.
             if prop_variant.vt != VT_LPWSTR {
